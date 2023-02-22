989d95b8da0553c3cc5faccc4611dbe0f71e3a22
Shailan Sheibani <shailansheibani@yahoo.com>
2023-02-22 21:18:17 +0000

    page done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,9 @@
 class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, presence: true
+  validates :category, inclusion: {
+    in: ["chinese", "italian", "japanese", "french", "belgian"],
+          message: "%{value} is not valid!!!" }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts 'cleaning database...'
+Restaurant.destroy_all
+puts 'destroyed restaurants'
+5.times do
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: %w[chinese italian japanese french belgian].sample
+  )
+  puts "Restaurant #{restaurant.name} was added!"
+end
